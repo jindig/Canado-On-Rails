fd18b8bb61a9194559c948742669ae538fe28e98
joshindig@gmail.com <joshindig@gmail.com>
2015-02-16 10:53:19 -0500

    Second git commit. Minor changes.

---
+++ app/views/todo_lists/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @todo_list, :id, :title, :string, :description, :text, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Canado::Application.routes.draw do
+  resources :todo_lists
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
