01bed60937e672be5ce656ce31bf1d078fcb568f
joshindig@gmail.com <joshindig@gmail.com>
2015-02-16 14:34:10 -0500

    Added changes to test ' create.rb' file

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+	it "displays error when title is left blank" do 
+		expect (TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page). to have_content("New todo_list")
+	
+		fill_in "Title", with: "My todo list"
+		fill_in "Description", with: "Here's what list contains."
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count). to eq(0)
+
+		
+		visit "/todo_lists"
+		expect(page).to_not have_content ("this is blah blah bullshit")
+
+	end
+
+end
