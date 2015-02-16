5d03dad6d5cdbb1e9cc627f3fe28f09855c7f588
joshindig@gmail.com <joshindig@gmail.com>
2015-02-16 14:50:06 -0500

    Added validations.

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,8 @@
 class TodoList < ActiveRecord::Base
+	validates  :title, presence: true
+	validates :title, length: {minimum:3}
+	validates  :description, presence: true
+	validates  :description, length: {minimum:5}
+
 end
+
