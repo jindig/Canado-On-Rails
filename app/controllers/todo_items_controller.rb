8ff784b6bb96264d2480298a83a50f2cb3c8397a
joshindig@gmail.com <joshindig@gmail.com>
2015-02-17 12:19:15 -0500

    A bunch of changes to make adding to do times possible.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,28 @@
 class TodoItemsController < ApplicationController
   def index
+  	@todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def new
+	@todo_list = TodoList.find(params[:todo_list_id])
+  	@todo_item = @todo_list.todo_items.new
+  end
+
+  def create
+  	@todo_list = TodoList.find(params[:todo_list_id])
+  	@todo_item = @todo_list.todo_items.new
+  	if todo_item,save
+  		flash[:success] = "You added a new todo list item."
+  		redirect_to todo_list_todo_items_path
+  	else
+  		flash[:error] = ":( - There was an error adding that todo list item. Try again!"
+  		render action: :new
+  	end
+  end
+
+  private
+  def todo_item_params
+  	params[:todo_item].permit(:content)
+  end
+
 end
