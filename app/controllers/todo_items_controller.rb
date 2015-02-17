c988baad623cb5f4365479b8ad4cf0af242edaa9
joshindig@gmail.com <joshindig@gmail.com>
2015-02-17 14:18:17 -0500

    Wayout layout updates.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,17 +1,16 @@
 class TodoItemsController < ApplicationController
+  before_action :find_todo_list
+
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
   end
 
   def new
-	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
-  	if todo_item.save
+  	if @todo_item.save
   		flash[:success] = "You added a new todo list item."
   		redirect_to todo_list_todo_items_path
   	else
@@ -21,15 +20,40 @@ class TodoItemsController < ApplicationController
   end
 
   def edit
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.find(params[:id])
   end
+
+  def update
+  	@todo_item = @todo_list.todo_items.find(params[:id])
+  	if @todo_item.update_atributes(todo_item_params)
+  		flash[:success] = "Saved todo list item."
+  		redirect_to todo_list_todo_items_path
+  	else
+  		flash[:error] = "Could not save todo list item. Try again!"
+  		render action: :edit
+  	end
+  end 
+
+  def destroy
+  		@todo_item = @todo_list.todo_items.find(params[:id])
+  		if @todo_item.destroy
+  			flash[:success] = "List item was deleted."
+  		else
+  			flash[:error] = "List item could not be deleted."
+  		end
+  		redirect_to todo_list_todo_items_path
+  	end
 
   def url_options
  	{ todo_list_id: params[:todo_list_id]}.merge(super)
   end
 
+
   private
+  def find_todo_list
+  	  @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
   def todo_item_params
   	params[:todo_item].permit(:content)
   end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,9 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
+
+  validates :content, presence: true,
+  								length { minimum: 2 }
+
+  
+
 end
